cf587413f1a3d47fcef25af4e9a02c21b67059be
Alberto-Zapardiel-Fernandez <alberto.zapardiel.fernandez@gmail.com>
2026-08-15 10:00:48 +0200

    Feat: Subida inicial

---
+++ src/test/java/com/futbol/majo/MajoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.futbol.majo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MajoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
